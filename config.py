fa33b86c9cd773f703cce415f583b50391c9b056
IMLHF <red_wind@foxmail.com>
2020-02-21 14:07:08 +0800

    delet nouse params

--- config.py
+++ config.py
@@ -5,8 +5,7 @@ workdir = '.'
 #
 # Network Optimization
 #
-lr = 1e-4       # Initial learning rate
-min_lr = 1e-5     # Learning rate minimum
+lr = 1e-3      # Initial learning rate
 
 #
 # Define short-term spectral parameters
@@ -16,17 +15,5 @@ n_window = 1024             # Window length
 stride = 64              # Step size
 selection = 9152            # sequence length
 
-#
-# Network training learning rate adjustment 
-# strategy and early stop strategy
-#
-patience = 40  # Endurance limit
-cooldown = 20   # The number of epochs that keep the learning rate constant
-factor = 0.4   # Learning rate decay factor
-start_it = 10  # The number of iterations at which the learning rate begins to adjust 
-cnt = 20        # Verify that the set loss value stops falling
-
-#
-# 
 #
 batch_size = 16
