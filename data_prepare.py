bc346f498dcb40342a46ec421d5611d3f5c110d6
IMLHF <red_wind@foxmail.com>
2020-02-21 17:19:22 +0800

    fix data_prepare

--- data_prepare.py
+++ data_prepare.py
@@ -2,7 +2,7 @@
 """
 2019-04-23
 """
-import os, sys
+import os
 import librosa
 import soundfile
 import argparse
@@ -11,6 +11,7 @@ import math
 import pickle
 import time
 import config as cfg
+from tqdm import tqdm
 
 def create_folder(fd):
   if not os.path.exists(fd):
@@ -36,64 +37,66 @@ def pack_waves(args):
   strides = slice_len // 2
   #window = np.hamming(slice_len)
 
-  val_set = 1500                       # Verification set data number
+  val_set = 100                       # Verification set data number
 
-  speech_names = [na.split(".")[0] for na in os.listdir(clean_dir) if na.lower().endswith('.wav')] 
+  speech_names = [na.split(".")[0] for na in os.listdir(clean_dir) if na.lower().endswith('.wav')]
   out_csv_path = os.path.join(workspace, "mixture_csvs")
   create_folder(out_csv_path)
-  pickle.dump(speech_names, 
-              open(os.path.join(out_csv_path, "train.csv"), 'wb'), 
+  pickle.dump(speech_names,
+              open(os.path.join(out_csv_path, "train.csv"), 'wb'),
               protocol=pickle.HIGHEST_PROTOCOL)
   np.random.seed()
   ids = np.random.permutation(len(speech_names))
   t1 = time.time()
   cnt = 0
-  index = ids[val_set: ]
+  index = ids[val_set:]
   out_path = os.path.join(workspace, "packs", "train_set")
   create_folder(out_path)
-  for i in index:
+  print("pack trainset:")
+  for i in tqdm(index):
     audio_clean, _ = read_audio(os.path.join(clean_dir, "%s.wav" % speech_names[i]))
     audio_noise, _ = read_audio(os.path.join(noisy_dir, "%s.wav" % speech_names[i]))
     n_samples = audio_clean.shape[0]
     slice_num = math.ceil((n_samples - slice_len) / strides) + 1
     for j in range(slice_num):
       if j * strides + slice_len > n_samples:
-        slice_clean = audio_clean[-slice_len: ]
-        slice_noise = audio_noise[-slice_len: ]
+        slice_clean = audio_clean[-slice_len:]
+        slice_noise = audio_noise[-slice_len:]
       else:
         slice_clean = audio_clean[j * strides: j * strides + slice_len]
         #slice_clean *= window
         slice_noise = audio_noise[j * strides: j * strides + slice_len]
         #slice_noise *= window
       data = [slice_clean, slice_noise]
-    
+
       pickle.dump(data, open(os.path.join(out_path, "%s_%d.p" % (speech_names[i], j)), 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
-      if cnt % 100 == 0:
-        print(cnt)
+      # if cnt % 100 == 0:
+      #   print(cnt)
       cnt += 1
   print("packing train set wave time: %s" % (time.time() - t1))
   out_path = os.path.join(workspace, "packs", "val_set")
   create_folder(out_path)
   index = ids[: val_set]
-  for i in index:
+  print("pack valset:")
+  for i in tqdm(index):
     audio_clean, _ = read_audio(os.path.join(clean_dir, "%s.wav" % speech_names[i]))
     audio_noise, _ = read_audio(os.path.join(noisy_dir, "%s.wav" % speech_names[i]))
     n_samples = audio_clean.shape[0]
     slice_num = math.ceil((n_samples - slice_len) / strides) + 1
     for j in range(slice_num):
       if j * strides + slice_len > n_samples:
-        slice_clean = audio_clean[-slice_len: ]
-        slice_noise = audio_noise[-slice_len: ]
+        slice_clean = audio_clean[-slice_len:]
+        slice_noise = audio_noise[-slice_len:]
       else:
         slice_clean = audio_clean[j * strides: j * strides + slice_len]
         #slice_clean *= window
         slice_noise = audio_noise[j * strides: j * strides + slice_len]
         #slice_noise *= window
       data = [slice_clean, slice_noise]
-    
+
       pickle.dump(data, open(os.path.join(out_path, "%s_%d.p" % (speech_names[i], j)), 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
-      if cnt % 100 == 0:
-        print(cnt)
+      # if cnt % 100 == 0:
+      #   print(cnt)
       cnt += 1
   print("packing total wave time: %s" % (time.time() - t1))
 
